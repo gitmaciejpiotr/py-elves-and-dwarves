94407ad9b8144adb87d73224be5c768e4cd66c17
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-01-19 15:53:19 +0100

    fixed errors

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
-def calculate_team_total_rating(team: list) -> int | float:
-    total_rating = 0
-    for member in team:
-        total_rating += member.get_rating()
+from app.players.player import Player
+from app.players.elves.elf import Elf
+from app.players.dwarves.dwarf import Dwarf
 
-    return total_rating
 
-def elves_concert(elves: list) -> None:
+def calculate_team_total_rating(team: list[Player]) -> int | float:
+    return sum(member.get_rating() for member in team)
+
+
+def elves_concert(elves: list[Elf]) -> None:
     for elf in elves:
         elf.play_elf_song()
 
-def feast_of_the_dwarves(dwarves: list) -> None:
+
+def feast_of_the_dwarves(dwarves: list[Dwarf]) -> None:
     for dwarf in dwarves:
         dwarf.eat_favourite_dish()
